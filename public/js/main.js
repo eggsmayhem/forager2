619e0e1815776c4cb78741bd225c8360be4b5995
eggsmayhem <flipmybit@protonmail.com>
2022-09-21 01:13:36 -0400

    some styling on map page

--- public/js/main.js
+++ public/js/main.js
@@ -41,8 +41,8 @@
         })
         
         const watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-              attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-              subdomains: 'abcd',
+              // attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+              // subdomains: 'abcd',
               minZoom: 1,
               maxZoom: 16,
               ext: 'jpg'
