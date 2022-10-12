e7ae3591329556412e5434dd52076ec702dd3477
eggsmayhem <flipmybit@protonmail.com>
2022-10-12 18:37:40 -0400

    serving leaflet js myself to avoid breaking issues

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+//set up static routes to serve leaflet css and js
+// app.use('/scripts', express.static(__dirname + '/node_modules/leaflet/dist/'));
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
